98f9477378757f3f9fb8ec2c626fe334200a5cf9
Ian Be <ian84be@gmail.com>
2019-01-28 17:45:46 -0600

    fill content cards & middle img

--- js/index.js
+++ js/index.js
@@ -47,6 +47,18 @@ const navLinkNames = Object
   .filter(e => e[0].includes('nav-item'))
   .map(e => e[1]);
 
+// GENERATE ARRAY FOR CONTENT TITLES
+const contentTitleArray = Object
+  .entries(siteContent['main-content'])
+  .filter(e => e[0].includes('h4'))
+  .map(e => e[1]);
+
+// GENERATE ARRAY FOR CONTENT TEXT
+const contentTextArray = Object
+  .entries(siteContent['main-content'])
+  .filter(e => e[0].includes('content'))
+  .map(e => e[1]);
+
 // POPULATE A TAGS
 let navLinks = document.querySelectorAll('nav a');
 let i = 0;
@@ -55,8 +67,30 @@ navLinks.forEach(element => {
     i++;
   });
 
+// CTA CONTENT
 document.querySelector('.cta-text h1').textContent = siteContent.cta.h1;
 document.querySelector('.cta-text button').textContent = siteContent.cta.button;
 document.getElementById('cta-img').setAttribute('src', siteContent["cta"]["img-src"]);
 
+// FILL CONTENT CARD TITLES
+let contentTitles = document.querySelectorAll('.text-content h4');
+i=0;
+contentTitles.forEach(element => {
+  element.textContent = contentTitleArray[i];
+  i++;
+});
 
+// FILL CONTENT CARD TEXT
+let contentText = document.querySelectorAll('.text-content p');
+i=0;
+console.log(contentText);
+contentText.forEach(element => {
+  element.textContent = contentTextArray[i];
+  i++;
+});
+
+
+// MIDDLE IMAGE
+let middleImg = document
+  .querySelector('.main-content img.middle-img')
+  .setAttribute('src', siteContent["main-content"]["middle-img-src"]);
